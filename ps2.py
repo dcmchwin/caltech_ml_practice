88813b4658c8c063470903abdabd8f33c5d5103e
David McHardy <dcm@uk.cambridgeconsultants.com>
2016-04-05 17:10:09 +0100

    work on code factoring

--- ps2.py
+++ ps2.py
@@ -1,63 +1,77 @@
+"""
+Module to hold homework set 2 problems.
+
+Problems requiring computational solutions are 1, 2.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-class Homework2(object):
-    """
-    Class to hold homework set 2 problems
-    Problems requiring computational solutions are 1, 2, 
-    """
-    @staticmethod
-    def heof(n_coins=1000, n_flips=10, n_experiments=100000):
-        """
-        covers problems 1 and 2 on the Heoffding Inequality
-        """
+def heof(n_coins=1000, n_flips=10, n_experiments=100000):
+    """Solve problems 1 and 2 on the Heoffding Inequality."""
+    # create array to hold all coins and all flips for one
+    # experiment
+    sz = (n_coins, n_flips)
 
-        # create array to hold all coins and all flips for one
-        # experiment
-        sz = (n_coins, n_flips)
+    # distributions
+    nu_firsts = np.zeros(n_experiments)
+    nu_rands = np.zeros(n_experiments)
+    nu_mins = np.zeros(n_experiments)
 
-        # distributions
-        nu_firsts = np.zeros(n_experiments)
-        nu_rands = np.zeros(n_experiments)
-        nu_mins = np.zeros(n_experiments)
+    print 'Start...'
 
-        print 'Start...'
+    for i in range(0, n_experiments):
 
-        for i in range(0, n_experiments):
+        rands = np.random.rand(*sz)
 
-            rands = np.random.rand(*sz)
+        coins = np.zeros(sz)
+        coins[rands > 0.5] = 1
 
-            coins = np.zeros(sz)
-            coins[rands > 0.5] = 1
+        nu = np.average(coins, 1)
 
-            nu = np.average(coins, 1)
+        nu_first = nu[0]
 
-            nu_first = nu[0]
+        rnd_idx = np.round((n_coins - 1) * np.random.rand())
+        nu_rand = nu[rnd_idx]
 
-            rnd_idx = np.round((n_coins - 1) * np.random.rand())
-            nu_rand = nu[rnd_idx]
+        nu_min = np.ndarray.min(nu)
 
-            nu_min = np.ndarray.min(nu)
+        # add to distributions
+        nu_firsts[i] = np.around(nu_first, 2)
+        nu_rands[i] = np.around(nu_rand, 2)
+        nu_mins[i] = np.around(nu_min, 2)
 
-            # add to distributions
-            nu_firsts[i] = np.around(nu_first, 2)
-            nu_rands[i] = np.around(nu_rand, 2)
-            nu_mins[i] = np.around(nu_min, 2)
+    print 'Average of nu_min =', np.average(nu_mins)
 
-        print 'Average of nu_min =', np.average(nu_mins)
-
-        # plot distributions
-        plt.hist(nu_firsts, bins=10, histtype='stepfilled')
-        plt.hist(nu_rands, bins=10, histtype='stepfilled', alpha=0.67)
-        plt.hist(nu_mins, bins=10, histtype='stepfilled', alpha=0.33)
-        plt.show()
-
-    @staticmethod
-    def err_and_noise():
-        """
-        covers problems 3 and 4 on error and err_and_noise
-        """
+    # plot distributions
+    plt.hist(nu_firsts, bins=10, histtype='stepfilled')
+    plt.hist(nu_rands, bins=10, histtype='stepfilled', alpha=0.67)
+    plt.hist(nu_mins, bins=10, histtype='stepfilled', alpha=0.33)
+    plt.show()
 
 
-Homework2.heof()
+def lin_reg():
+    """Solve problems 5 and 6 on Linear Regression."""
+    def classifier(coef):
+        def map(dat):
+            in_prod = np.inner(dat, coef)
+            return (in_prod > 0) * 2 - 1
+        return map
+    x = np.array([1, 1, 1])
+    f = classifier(np.array([1, 1, 1]))
+    g = classifier(np.array([-1, -1, -1]))
+    print(f(x))
+    print(g(x))
+
+
+def perform_chosen_function(choice=0):
+    """Perform main."""
+    if choice == 0:
+        heof()
+    elif choice == 1:
+        lin_reg()
+
+
+if __name__ == "__main__":
+    perform_chosen_function(choice=1)
---
+++ vector.py
@@ -0,0 +1,50 @@
+"""Module to hold mathematical vector utilities."""
+import numpy as np
+import 'U:\\Training\\Python practice\\machine learning\\utils\\vector.py'
+
+class MyError(Exception):
+    """Define custom exceptions."""
+
+    def __init__(self, value):
+        """Define init method."""
+        self.value = value
+
+    def __str__(self):
+        """Define string representation."""
+        return repr(self.value)
+
+
+def n_d_cross(*args):
+    """
+    Return d-dimensional cross product of vectors.
+
+    input: d-1 numpy vectors of dimension d
+    """
+
+    d = len(args[0])
+    n = len(args)
+
+    # error check
+    if n != d - 1:
+        raise MyError("Number of input vectors must be one \
+            less than vector dimension")
+
+    # initialise cross product output
+    v = np.zeros(d)
+
+    # define tuple of matrices
+    for i in range(0, d):
+        A = np.vstack([np.append(a[:i], a[i + 1:]) for a in args])
+        v[i] = np.linalg.det(A)
+
+    return v
+
+if __name__ == "__main__":
+    test = np.array([1, 0, 0])
+    test2 = np.array([0, 0, 1])
+
+    l = (test, test2)
+
+    vec = n_d_cross(*l)
+
+    print test, " x ", test2, " = ", vec
